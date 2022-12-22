6c69a476df954a94c844fd5d86077ce8c7608c23
Peregrine05 <92183530+Peregrine05@users.noreply.github.com>
2022-12-22 22:16:02 +0100

    Update the thumbnail script
    - Use `!=` instead of `is not`.
    - Convert all images into RGB format to allow for proper saving as JPEG.

--- tools/thumbnail.py
+++ tools/thumbnail.py
@@ -2,14 +2,14 @@ from PIL import Image
 from PIL import ImageOps
 import os, sys
 
-if len(sys.argv) is not 3:
+if len(sys.argv) != 3:
     print('Usage: thumbnail.py [input] [output]')
     sys.exit(1)
 
 input = sys.argv[1]
 output = sys.argv[2]
 try:
-	img = Image.open(input)
+	img = Image.open(input).convert("RGB")
 	img = ImageOps.fit(img, (256, 256), Image.ANTIALIAS)
 	img.save(output, 'JPEG')
 except IOError as e:
